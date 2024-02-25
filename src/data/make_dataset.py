7297034a1cdeb9c9b0412626ccada7ce4f18212e
Deniz Nuhoglu <dnuhoglu@gmail.com>
2024-02-26 01:40:28 +0300

    Commit make_dataset.py

---
+++ src/data/make_dataset.py
@@ -0,0 +1,65 @@
+import pandas as pd
+import pickle
+
+data_folder = '../../data'
+
+def make_dataset(data_folder:str) -> list([pd.DataFrame(), pd.DataFrame()]):
+
+    # Set data types for pandas to load csv
+    dtype={
+                                'Store':'Int64'
+                                ,'DayOfWeek':'Int64'
+                                ,'Date':'str'
+                                ,'Sales':'Int64'
+                                ,'Customers':'Int64'
+                                ,'Open':'Int64'
+                                ,'Promo':'Int64'
+                                ,'StateHoliday':'str'
+                                ,'SchoolHoliday':'Int64'
+                            }
+
+    data_train = pd.read_csv(data_folder+'/raw/train.csv'
+                            ,dtype=dtype
+                            ,parse_dates=['Date']
+    )
+
+    data_test = pd.read_csv(data_folder+'/raw/test.csv'
+                            ,dtype=dtype
+                            ,parse_dates=['Date']
+                            ,index_col='Id'
+    )
+
+    sample_submission = pd.read_csv(data_folder+'/raw/sample_submission.csv')
+    data_store = pd.read_csv(data_folder+'/raw/store.csv')
+
+    # Dropping fields DayOfWeek (redundant), Customers (not needed), 'Open' filter is not needed as well since we only train and predict open days
+    # Filter necessary fields
+
+    data_train = data_train[data_train['Open']==1][['Store','Date','Promo','StateHoliday','SchoolHoliday','Sales']]
+    data_test = data_test[data_test['Open']==1][['Store','Date','Promo','StateHoliday','SchoolHoliday']]
+    data_train = data_train[['Store','Date','Promo','StateHoliday','SchoolHoliday','Sales']]
+    data_test = data_test[['Store','Date','Promo','StateHoliday','SchoolHoliday']]
+
+    # data_train.rename({'Sales':'y','Date':'ds'},axis=1, inplace=True)
+    # data_test.rename({'Sales':'y','Date':'ds'},axis=1, inplace=True)
+
+    data_train.set_index(['Store','Date'], inplace=True)
+    data_test.set_index(['Store','Date'], inplace=True)
+
+    data_train.index.levels[1].freq='D'
+    data_test.index.levels[1].freq='D'
+
+    data_train.sort_index(level=[0,1],inplace=True)
+    data_test.sort_index(level=[0,1],inplace=True)
+
+    print(f'Make dataset completed!')
+    
+    
+    data_train.to_parquet(data_folder+'/interim/df_train.parquet')
+    data_test.to_parquet(data_folder+'/interim/df_test.parquet')
+
+    return data_train, data_test
+
+if __name__ == "__main__":
+
+    make_dataset(data_folder)
