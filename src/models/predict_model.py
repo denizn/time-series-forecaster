42449ec17aaed38ea655ca304308a9902351416a
Deniz Nuhoglu <dnuhoglu@gmail.com>
2024-02-27 03:54:34 +0300

    Polish predict model script and functions

--- src/models/predict_model.py
+++ src/models/predict_model.py
@@ -3,25 +3,31 @@ import pandas as pd
 from prophet.plot import plot_plotly, plot_components_plotly
 
 parser = argparse.ArgumentParser()
-parser.add_argument('store_number', type=int)
+parser.add_argument('-s', '--store_number', type=int)
 args = parser.parse_args()
 
-def get_prediction(store_number=1):
+def get_prediction(store_number, only_forecast=True):
     '''
     This function gets predictions for the requested store number
     returns: pd.DataFrame
     '''
     
-    df_test = pd.read_parquet('../../data/processed/df_test_X.parquet').loc[store_number].reset_index()
+    df_test_large = pd.read_parquet('data/processed/df_test_X.parquet')
 
-    with open('../../models/saved_models/1.pkl','rb') as handle:
+    if store_number not in (df_test_large.index.levels[0]):
+        raise Exception("Store number not found in test dataset")
+    
+    df_test = df_test_large.loc[store_number].reset_index()
+
+    with open('models/saved_models/1.pkl','rb') as handle:
         model = pickle.load(handle)
         prediction = model.predict(df_test)
 
-    plot_components_plotly(model, model.predict(df_test))
+    fig = plot_components_plotly(model, model.predict(df_test))
+    fig.write_html(f'models/components/{store_number}.html')
 
-    return prediction
+    return prediction.set_index('ds')['yhat'] if only_forecast else prediction
 
 if __name__ == '__main__':
-    yhat = get_prediction(store_number=args.store_number if args.store_number else None).set_index('ds')['yhat']
+    yhat = get_prediction(store_number=args.store_number if args.store_number else 1).set_index('ds')['yhat']
     print(yhat)
